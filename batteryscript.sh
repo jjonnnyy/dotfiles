43763d2a9f6b026603534688e9ab9464881d3f2b
Jonathan Ely <me@jonathanely.com>
2012-06-30 17:21:45 +0100

    Initial commit

---
+++ batteryscript.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+while :
+do
+	# load battery state
+	charge_now=`cat /sys/class/power_supply/BAT1/charge_now`
+	charge_full=`cat /sys/class/power_supply/BAT1/charge_full`
+	percent=$[$charge_now*100/$charge_full]
+	# load charging state
+	pluggedin=`cat /sys/class/power_supply/BAT1/status`
+
+if [ "$pluggedin" != "Discharging" ]; then
+		# Check first if charging
+		sleep 5m
+	elif [ "$percent" -lt "15" ]; then
+		# If below 15% hibernate
+		notify-send -u critical "HIBERNATING DUE TO LOW BATTERY"
+		sudo pm-hibernate
+		sleep 3m
+	elif [ "$percent" -lt "25" ]; then
+		# If below 25% alert user
+		notify-send -u critical "LOW BATTERY PLUG IN NOW"
+		sleep 2m	
+	elif [ "$percent" -lt "60" ]; then
+		# If below 50% check more regularly
+		notify-send -u low "Battery at $percent%"
+		sleep 8m	
+	else
+		# Else above 50% wait awhile
+		sleep 16m
+	fi
+done
---
+++ vimrc
@@ -0,0 +1,15 @@
+filetype plugin on
+syntax on
+
+" Spell Check "
+set spelllang=en_gb
+
+" Tabbing "
+set expandtab
+set shiftwidth=4
+set tabstop=4
+set smarttab
+
+set ai "Auto indent
+set si "Smart indet
+set wrap "Wrap lines
