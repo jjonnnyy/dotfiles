8531f989605471e802948c369ffdf894b59afb09
Jonathan Ely <me@jonathanely.com>
2012-07-03 16:17:05 +0100

    Added notifications back into batteryscript

--- batteryscript.sh
+++ batteryscript.sh
@@ -11,19 +11,25 @@ do
 if [ "$pluggedin" != "Discharging" ]; then
 		# Check first if charging
         echo "" > .chargenow
-		sleep 5m
+        sleep 5m
 	elif [ "$percent" -lt "15" ]; then
 		# If below 15% hibernate
         echo "<fc=#FF0000>HIBERNATING DUE TO LOW BATTERY</fc>" > .chargenow
+        notify-send -u critical "HIBERNATING DUE TO LOW BATTERY"
 		sudo pm-hibernate
 		sleep 3m
 	elif [ "$percent" -lt "25" ]; then
 		# If below 25% alert user
-		echo "<fc=#FF0000>LOW BATTERY CHARGE NOW</fc>" > .chargenow 
-        sleep 2m	
+        echo "<fc=#FF0000>LOW BATTERY CHARGE NOW</fc>" > .chargenow
+		notify-send -u critical "LOW BATTERY PLUG IN NOW"
+		sleep 2m
+    elif [ "$percent" -lt "60" ]; then
+		# If below 50% check more regularly
+		notify-send -u low "Battery at $percent%"
+		sleep 8m
 	else
 		# Else above 50% wait awhile
-		echo "" > .chargenow
-        sleep 15m
+        echo "" > .chargenow        
+		sleep 16m
 	fi
 done
