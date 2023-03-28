d54822fde8110fd575f254a9d7aa0bb4a9a19e95
Jonathan Ely <me@jonathanely.com>
2023-03-28 19:25:13 +0100

    Wezterm config update

--- wezterm.lua
+++ wezterm.lua
@@ -25,7 +25,28 @@ config.keys = {
     key = '5',
     mods = 'CMD',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '.',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
   }
+}
+
+config.window_frame = {
+  -- The size of the font in the tab bar.
+  -- Default to 10. on Windows but 12.0 on other systems
+  font_size = 12.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  -- work '#147D64'
+  -- personal '#4C63B6'
+  active_titlebar_bg = '#4C63B6',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#4C63B6',
 }
 
 -- and finally, return the configuration to wezterm
